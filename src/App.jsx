e4ffa6c27908daf74d57f77eb3221698e90ac634
JRReduta <jrreduta@yahoo.com>
2025-07-23 18:37:08 -0700

    Did tasks #1-4

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Register from "./auth/Register";
 import Login from "./auth/Login";
 import ActivitiesPage from "./activities/ActivitiesPage";
 import Error404 from "./Error404.jsx";
+import { Route, Routes } from "react-router";
+import Layout from "./layout/Layout.jsx";
 
 /**
  * Fitness Trackr is a platform where fitness enthusiasts can share their workouts and
@@ -11,11 +13,13 @@ import Error404 from "./Error404.jsx";
  * account will be able to upload and manage their own activities.
  */
 export default function App() {
-  const { page } = usePage();
-
-  if (page === "register") return <Register />;
-  if (page === "login") return <Login />;
-  if (page === "activities") return <ActivitiesPage />;
-
-  return <Error404 />;
+  return (
+    <Routes>
+      <Route element={<Layout />} />
+      <Route index element={<ActivitiesPage />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<Error404 />} />
+    </Routes>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,16 @@ import Layout from "./layout/Layout";
 import { AuthProvider } from "./auth/AuthContext";
 import { ApiProvider } from "./api/ApiContext";
 import { PageProvider } from "./layout/PageContext";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <ApiProvider>
-      <PageProvider>
-        <Layout>
-          <App />
-        </Layout>
-      </PageProvider>
+      {/* <Layout> TODO: should we actually remove this? */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      {/* </Layout> */}
     </ApiProvider>
-  </AuthProvider>,
+  </AuthProvider>
 );
