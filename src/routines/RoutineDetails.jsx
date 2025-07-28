af7b41b3a5d8a13e71f192130292bd30889813ec
JRReduta <jrreduta@yahoo.com>
2025-07-27 23:53:54 -0700

    added routine list and details

--- src/routines/RoutineDetails.jsx
+++ src/routines/RoutineDetails.jsx
@@ -1,13 +1,36 @@
+import { useNavigate, useParams } from "react-router";
 import { useAuth } from "../auth/AuthContext";
 import SetForm from "../sets/SetForm";
 import SetList from "../sets/SetList";
+import { errorUrl } from "../urls/urls";
+import { useEffect } from "react";
+import useQuery from "../api/useQuery";
 
 export default function RoutineDetails() {
+  const { id } = useParams();
+
   const { token } = useAuth();
-  return (
-    <>
-      <SetList />
-      {token && <SetForm />}
-    </>
-  );
+  const { data, loading, error } = useQuery("/routines/" + id, "routines");
+  const navigateTo = useNavigate();
+  useEffect(() => {
+    if (error) {
+      navigateTo(errorUrl);
+    }
+  }, [navigateTo, error]);
+
+  if (loading) {
+    return <div className="loading">Loading...</div>;
+  }
+  if (data) {
+    const { name, goal, creatorName } = data;
+    return (
+      <div className="routine-details">
+        <div>{name}</div>
+        <div>{goal}</div>
+        <div>{creatorName}</div>
+        <SetList data={data} />
+        {token && <SetForm />}
+      </div>
+    );
+  }
 }
--- src/routines/RoutineList.jsx
+++ src/routines/RoutineList.jsx
@@ -1,10 +1,32 @@
 import { Link } from "react-router";
 import { routinesUrl } from "../urls/urls";
+import useQuery from "../api/useQuery";
 
 export default function RoutineList() {
+  const { data: routines, loading, error } = useQuery("/routines", "routines");
+
+  if (loading || !routines) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Sorry! {error}</p>;
+  }
+
   return (
-    <div>
-      <Link to={`${routinesUrl}/1`}>TODO: Routine List</Link>
-    </div>
+    <ul>
+      {routines.map((routine) => (
+        <RoutineListItem key={routine.id} routine={routine} />
+      ))}
+    </ul>
+  );
+}
+
+function RoutineListItem({ routine }) {
+  return (
+    <li>
+      <div>
+        <Link to={`${routinesUrl}/${routine.id}`}>{routine.name}</Link>
+      </div>
+    </li>
   );
 }
